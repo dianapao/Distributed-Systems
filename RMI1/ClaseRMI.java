a746cc4a20efe6019aea08fadd8f9679285b4d36
Diana <dianapaodcs@gmail.com>
2022-03-21 16:16:32 -0600

    Ejemplo RMI (mayusculas, suma, checksum)

---
+++ RMI1/ClaseRMI.java
@@ -0,0 +1,27 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class ClaseRMI extends UnicastRemoteObject implements InterfaceRMI{
+    //Es necesario que el constructor ClaseRMI() invoque al constructor de la superclase
+
+    public ClaseRMI() throws RemoteException{
+        super();
+    }
+
+    public String mayusculas(String s) throws RemoteException{
+        return s.toUpperCase();
+    }
+
+    public int suma(int a, int b){
+        return a + b;
+    }
+
+    public long checksum(int[][] m) throws RemoteException{
+        long s = 0;
+        for(int i = 0; i < m.length; i++)
+            for(int j = 0; j < m[0].length; j++)
+                s += m[i][j];
+        return s;
+    }
+
+}
